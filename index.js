9a7f3f2f1c73be8539e165fc0b33673fbda5bdb0
matheuefranco <matheusefranco@gmail.com>
2024-03-21 18:15:44 -0300

    modelo

---
+++ index.js
@@ -0,0 +1,59 @@
+ // Crie uma instância da fila
+ let minhaFila = new FilaCircular(5);
+
+ // Função para adicionar um elemento à fila
+ function adicionarElemento() {
+     
+ }
+//--------------------------------------------------------------------------------------------
+ // Função para remover o primeiro elemento da fila
+ function removerElemento() {
+ 
+ }
+ //--------------------------------------------------------------------------------
+ function buscarCpf() {
+    
+}
+//--------------------------------------------------------------------------------------------
+function mostrarMensagemRemocao(pessoaAtendida) {
+    
+}
+//--------------------------------------------------------------------------------------------
+ // Função para atualizar a exibição da fila
+ function atualizarFila() {
+     
+    }
+
+ 
+//--------------------------------------------------------------------------------------------
+ // funcao data
+ function obterDataAtual() {
+    let dataAtual = new Date();
+    let dia = dataAtual.getDate();
+    let mes = dataAtual.getMonth() + 1; // Adiciona 1 porque o mês inicia do zero
+    let ano = dataAtual.getFullYear();
+    // Formata a data como "dd/mm/aaaa"
+    let dataFormatada = `${dia.toString().padStart(2, '0')}/${mes.toString().padStart(2, '0')}/${ano}`;
+    return dataFormatada;
+}
+//--------------------------------------------------------------------------------------------
+function obterHoraAtual() {
+  const data = new Date();
+  const hora = data.getHours().toString().padStart(2, '0');
+  const minuto = data.getMinutes().toString().padStart(2, '0');
+  const segundo = data.getSeconds().toString().padStart(2, '0');
+  return `${hora}:${minuto}:${segundo}`;
+}
+//--------------------------------------------------------------------------------------------
+function calcularDiferencaHoras(hora1, hora2) {
+  const [h1, m1, s1] = hora1.split(':').map(Number);
+  const [h2, m2, s2] = hora2.split(':').map(Number);
+  
+  const diferencaSegundos = (h2 * 3600 + m2 * 60 + s2) - (h1 * 3600 + m1 * 60 + s1);
+  
+  const horas = Math.floor(diferencaSegundos / 3600);
+  const minutos = Math.floor((diferencaSegundos % 3600) / 60);
+  const segundos = diferencaSegundos % 60;
+  
+  return `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+}
